90dbd06e7b359c2dd922675d47a837ddaea6fd62
isobolewski <is@sobolewski.cz>
2020-11-05 20:31:16 +0100

    Remove global MiniStats depencenies

--- src/main.js
+++ src/main.js
@@ -4,14 +4,9 @@ import router from './router';
 import store from './store';
 
 import Axios from 'axios';
-
 import * as pc from 'playcanvas';
-import * as pcx from 'playcanvas/build/playcanvas-extras';
 
-window.pc = pc; // Used for the MiniStats (pcx)
-
-Vue.prototype.$pc = window.pc;
-Vue.prototype.$pcx = pcx;
+Vue.prototype.$pc = pc;
 Vue.prototype.$axios = Axios;
 
 Vue.config.productionTip = false
