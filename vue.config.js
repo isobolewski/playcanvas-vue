beda136fc907c5935a5317a6e0b1f5476f123ac1
isobolewski <is@sobolewski.cz>
2020-11-07 23:35:30 +0100

    Fix: Format of vue.config

--- vue.config.js
+++ vue.config.js
@@ -7,48 +7,27 @@ module.exports = {
     configureWebpack: config => {
         config.optimization = {
             minimize: false
-        },
+        };
 
-            config.devServer = {
-                compress: true, // Use GZip compression
-                before(app) {
-                    // use proper mime-type for wasm files
-                    app.get('*.wasm', function (req, res, next) {
-                        const options = {
-                            root: contentBase + '/public/',
-                            dotfiles: 'deny',
-                            headers: {
-                                'Content-Type': 'application/wasm'
-                            }
-                        };
-                        res.sendFile(req.url, options, function (err) {
-                            if (err) { next(err); }
-                        });
-                    });
-                }
-            }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // options...
-    /*
-    configureWebpack: {
-        devServer: {
+        config.devServer = {
             compress: true, // Use GZip compression
-            mimeTypes: {},
+            before(app) {
+                // use proper mime-type for wasm files
+                app.get('*.wasm', function (req, res, next) {
+                    const options = {
+                        root: contentBase + '/public/',
+                        dotfiles: 'deny',
+                        headers: {
+                            'Content-Type': 'application/wasm'
+                        }
+                    };
+                    res.sendFile(req.url, options, function (err) {
+                        if (err) { next(err); }
+                    });
+                });
+            },
+
             // open: 'Google Chrome' // Start Google Chrome after npm run serve
-        }
-    }*/
+        };
+    }
 }
